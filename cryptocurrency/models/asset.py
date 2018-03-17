9b66d8027aae8e15c33f46b1bce18823cf8aa5f9
Martin Trigaux <mat@odoo.com>
2018-03-17 10:24:00 +0100

    [ADD] crypto: display name

--- cryptocurrency/models/asset.py
+++ cryptocurrency/models/asset.py
@@ -6,6 +6,8 @@ from odoo.addons import decimal_precision as dp
 
 class ResCurrencyAsset(models.Model):
     _name = "crypto.currency.asset"
+    _order = "currency_id"
+    _rec_name = 'display_name'
 
 
     @api.depends('currency_id', 'quantity', 'company_id')
@@ -13,9 +15,10 @@ class ResCurrencyAsset(models.Model):
         for asset in self:
             if not asset.currency_id or not asset.company_currency_id:
                 continue
-            asset.value = asset.currency_id.compute(
-                asset.quantity, asset.company_currency_id)
+            asset.value = asset.currency_id.compute(asset.quantity, asset.company_currency_id)
+            asset.display_name = "%s (%s)" % (asset.currency_id.name, asset.value)
 
+    display_name = fields.Char(compute='_get_asset_value')
     currency_id = fields.Many2one('res.currency', string="Asset")
     quantity = fields.Float()
 
